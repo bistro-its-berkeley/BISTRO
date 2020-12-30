1cc4510dfc7bcf6385fa155128fed4d052acc348
chenjia-christina-lu <72430310+chenjia-christina-lu@users.noreply.github.com>
2020-12-29 20:33:21 -0800

    Clean output helper func

--- BISTRO-Optimization-Library/per_mile/optimizer_per_mile_freeform.py
+++ BISTRO-Optimization-Library/per_mile/optimizer_per_mile_freeform.py
@@ -132,56 +132,8 @@ def objective(params):
     output_dir = only_subdir(only_subdir(output_dir))
     shutil.copy(os.path.join(output_dir, *SCORES_PATH), input_dir)
     
-    # Start: Clean output folder
-    clean_dir = str(output_dir)
-
-    # Set folder paths
-    iters_folder = clean_dir + "/ITERS"
-    competition_folder = clean_dir + "/competition"
-    summary_folder = clean_dir + "/summaryStats"
-
-    # Remove excess root folder files
-    file_list = [f for f in listdir(clean_dir) if isfile(join(clean_dir, f))]
-    keep_files = ["outputEvents.xml.gz", "realizedModeChoice.csv", "summaryStats.csv"]
-    for file in file_list:
-        if file not in keep_files:
-            if os.path.exists(clean_dir + "/" + file):
-                file_path = clean_dir + "/" + file
-                os.remove(file_path)
-
-    # Remove excess competition files
-    if os.path.exists(competition_folder + "/submission-inputs"):
-        shutil.rmtree(competition_folder + "/submission-inputs")
-    if os.path.exists(competition_folder + "/submissionScores.csv"):
-        os.remove(competition_folder + "/submissionScores.csv")
-    if os.path.exists(competition_folder + "/validation-errors.out"):
-        os.remove(competition_folder + "/validation-errors.out")
-
-    # Remove files in competition/viz folder
-    viz_folder = competition_folder + "/viz"
-    file_list = [f for f in listdir(clean_dir) if isfile(join(viz_folder, f))]
-    keep_files = ["link_stats.csv"]
-    for file in file_list:
-        if file not in keep_files:
-            if os.path.exists(clean_dir + "/" + file):
-                file_path = viz_folder + "/" + file
-                os.remove(file_path)
-
-    # Remove summary stats directory
-    if os.path.exists(summary_folder):
-        shutil.rmtree(summary_folder)
-
-    # Remove excess iter files
-    iter_list = os.listdir(iters_folder)
-    for folder in iter_list:
-        folder_path = iters_folder + "/" + folder
-        if os.path.exists(folder_path + "/tripHistogram"):
-            shutil.rmtree(folder_path + "/tripHistogram")
-        file_list = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
-        for file in file_list:
-            if file.endswith('events.xml.gz') == False:
-                os.remove(folder_path + "/" + file)
-    # End: Clean output files
+    #Clean output folder
+    clean_output(output_dir)
     
     paths = (input_dir, output_dir)
 
@@ -198,7 +150,55 @@ def objective(params):
     return {'loss': loss, 'params': params, 
             'train_time': run_time, 'status': STATUS_OK, 'paths': paths}
 
+def clean_output(output_dir):
+    path = str(output_dir)
 
+    # Set folder paths
+    iters_folder = path + "/ITERS"
+    competition_folder = path + "/competition"
+    summary_folder = path + "/summaryStats"
+
+    # Remove excess root folder files
+    file_list = [f for f in listdir(path) if isfile(join(path, f))]
+    keep_files = ["outputEvents.xml.gz", "realizedModeChoice.csv", "summaryStats.csv"]
+    for file in file_list:
+        if file not in keep_files:
+        	if os.path.exists(path + "/" + file):
+	            file_path = path + "/" + file
+	            os.remove(file_path)
+
+    # Remove excess competition files
+    if os.path.exists(competition_folder + "/submission-inputs"):
+        shutil.rmtree(competition_folder + "/submission-inputs")
+    if os.path.exists(competition_folder + "/submissionScores.csv"):
+        os.remove(competition_folder + "/submissionScores.csv")
+    if os.path.exists(competition_folder + "/validation-errors.out"):
+        os.remove(competition_folder + "/validation-errors.out")
+
+    # Remove files in competition/viz folder
+    viz_folder = competition_folder + "/viz"
+	file_list = [f for f in listdir(path) if isfile(join(viz_folder, f))]
+	keep_files = ["link_stats.csv"]
+    for file in file_list:
+	    if file not in keep_files:
+	    	if os.path.exists(path + "/" + file):
+		        file_path = viz_folder + "/" + file
+		        os.remove(file_path)
+
+    # Remove summary stats directory
+    if os.path.exists(summary_folder):
+        shutil.rmtree(summary_folder)
+
+    # Remove excess iter files
+    iter_list = os.listdir(iters_folder)
+    for folder in iter_list:
+        folder_path = iters_folder + "/" + folder
+        if os.path.exists(folder_path + "/tripHistogram"):
+            shutil.rmtree(folder_path + "/tripHistogram")
+        file_list = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
+        for file in file_list:
+            if file.endswith('events.xml.gz') == False:
+                os.remove(folder_path + "/" + file)
 
 def get_score(output_dir):
     standards = load_standards()
