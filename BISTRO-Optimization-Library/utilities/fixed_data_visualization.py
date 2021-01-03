4aca9d9f181a132b2d24854125e4a583f6ec7ee2
chenjia-christina-lu <72430310+chenjia-christina-lu@users.noreply.github.com>
2021-01-02 18:03:16 -0800

    Database helper functions

---
+++ BISTRO-Optimization-Library/utilities/fixed_data_visualization.py
@@ -0,0 +1,125 @@
+from pathlib import Path
+import pandas as pd
+import visualization as viz
+import plans_parser as parser
+
+
+REFERENCE_DATA = "reference-data"
+AGENCY = "sioux_faux_bus_lines"
+CONFIG = "config"
+EXAMPLES = "examples"
+COMPETITION = "competition"
+SUBMISSION_INPUTS = "submission-inputs"
+ITERS = "ITERS"
+
+
+max_incentive = 50
+max_income = 150000
+max_age = 120
+max_fare = 10
+transit_scale_factor = 0.1
+
+poi_types = ['work', 'secondary']
+time_ranges = {'morning peak': range(7, 10), "evening peak": range(17, 20)}
+max_time = 900
+utm_zone = "14N"
+
+
+class ReferenceData(object):
+
+    def __init__(self, sample_size, scenario_name="sioux_faux", transit_scale_factor=0.1):
+        """
+        Parameters
+        ----------
+        reference_data_path : Path
+            Directory containing the scenario's reference-data
+        """
+        # Scale factor to compare the 15k to the 157k scenario (full population)
+        self.transit_scale_factor = transit_scale_factor
+
+        # Importing agencies ids from agency.txt
+        agency_ids = pd.read_csv(Path.cwd().parent / REFERENCE_DATA / scenario_name / AGENCY / "gtfs_data/agency.txt")
+        self.agency_ids = agency_ids["agency_id"].tolist()
+
+        # Importing route ids from `routes.txt`
+        route_df = pd.read_csv(Path.cwd().parent / REFERENCE_DATA / scenario_name / AGENCY /"gtfs_data/routes.txt")
+        self.route_ids = route_df["route_id"].sort_values(ascending=True).tolist()
+
+        # Importing vehicle types and seating capacities from `availableVehicleTypes.csv` file
+        self.available_vehicle_types = pd.read_csv(Path.cwd().parent / REFERENCE_DATA /
+                                                   scenario_name / AGENCY /"availableVehicleTypes.csv")
+
+        self.buses_list = self.available_vehicle_types["vehicleTypeId"][1:].tolist()
+        self.seating_capacities = self.available_vehicle_types[["vehicleTypeId", "seatingCapacity"]].\
+                                                    set_index("vehicleTypeId", drop=True).T.to_dict("records")[0]
+
+        # Extracting Operational costs per bus type from the `vehicleCosts.csv` file
+        operational_costs = pd.read_csv(Path.cwd().parent / REFERENCE_DATA / scenario_name / AGENCY / "vehicleCosts.csv")
+        self.operational_costs = operational_costs[["vehicleTypeId", "opAndMaintCost"]].\
+                                                    set_index("vehicleTypeId", drop=True).T.to_dict("records")[0]
+
+        # Extracting route_id / trip_id correspondence from the `trips.csv` file
+        trips = pd.read_csv(Path.cwd().parent / REFERENCE_DATA / scenario_name / AGENCY / "gtfs_data/trips.txt")
+        self.trip_to_route = trips[["trip_id", "route_id"]].set_index("trip_id", drop=True).T.to_dict('records')[0]
+
+
+        #Extracting Fuel cost from the `beamFuelTypes.csv` file
+        fuel_costs = pd.read_csv(Path.cwd().parent / REFERENCE_DATA / scenario_name / CONFIG / sample_size / "beamFuelTypes.csv")
+        fuel_costs.loc[len(fuel_costs)] = ["food", 0]
+        self.fuel_costs = fuel_costs.set_index("fuelTypeId", drop=True).T.to_dict('records')[0]
+
+        # PATHS TO BAU DATA
+        # 1.ouput folder
+        path_output_folder_bau = (Path.cwd().parent / REFERENCE_DATA / scenario_name /
+                                  f"bau/warm-start/sioux_faux-{sample_size}__warm-start").absolute()
+        self.path_output_folder_bau = viz.unzip_file(path_output_folder_bau)
+
+        # 2. Network and population files
+        self.path_network_file = Path.cwd().parent / REFERENCE_DATA /scenario_name / CONFIG/ "physsim-network.xml"
+        self.path_population_file = Path.cwd().parent / REFERENCE_DATA /scenario_name / CONFIG / f"{sample_size}/population.xml.gz"
+
+
+class ResultFiles:
+    def __init__(self, path_output_folder, number_iterations, reference_data: ReferenceData):
+
+        self.path_output_folder = path_output_folder
+        self.number_iterations = number_iterations
+        self.reference_data = reference_data
+
+        # Extracting input data from the submission input csv files
+        self.bus_fares_data = pd.read_csv(path_output_folder / COMPETITION / SUBMISSION_INPUTS / "MassTransitFares.csv")
+        self.incentives_data = pd.read_csv(path_output_folder / COMPETITION / SUBMISSION_INPUTS / "ModeIncentives.csv")
+        self.fleet_mix_data = pd.read_csv(path_output_folder / COMPETITION / SUBMISSION_INPUTS / "VehicleFleetMix.csv")
+        self.bus_frequency_data = pd.read_csv(path_output_folder / COMPETITION / SUBMISSION_INPUTS / "FrequencyAdjustment.csv")
+
+        # Extracting output data
+        self.scores_data = pd.read_csv(path_output_folder/ COMPETITION / "submissionScores.csv")
+        self.mode_choice_data = pd.read_csv(path_output_folder / "modeChoice.csv")
+
+        # Getting the csv data from the xml files
+        self.process_all_xml_files()
+
+    def process_all_xml_files(self):
+        """ Import all xml.gz files from the output folder of the scenario, parse them and create .csv files"
+        """
+
+        self.events_path = self.path_output_folder / ITERS / "it.{0}".format(self.number_iterations) / \
+                           "{0}.events.csv.gz".format(self.number_iterations)
+        self.output_plans_path = self.path_output_folder / "outputPlans.xml.gz"
+        self.experienced_plans_path = self.path_output_folder / ITERS / "it.{0}".format(self.number_iterations) \
+                                      / "{0}.experiencedPlans.xml.gz".format(self.number_iterations)
+        self.persons_path = self.path_output_folder / "outputPersonAttributes.xml.gz"
+        self.households_path = self.path_output_folder / "outputHouseholds.xml.gz"
+
+        if not Path(self.path_output_folder / "trips_dataframe.csv").exists():
+            # Parsing and creating the csv files in the output folder
+            parser.output_parse(self.events_path, self.output_plans_path, self.persons_path, self.households_path,
+                                self.experienced_plans_path, self.bus_fares_data, self.reference_data.route_ids,
+                                self.reference_data.trip_to_route, self.reference_data.fuel_costs, self.path_output_folder)
+
+        # Get the data from the generated csv files
+        self.trips_df = pd.read_csv(self.path_output_folder / "trips_dataframe.csv")
+        self.person_df = pd.read_csv(self.path_output_folder / "persons_dataframe.csv")
+        self.activities_df = pd.read_csv(self.path_output_folder / "activities_dataframe.csv")
+        self.legs_df = pd.read_csv(self.path_output_folder / "legs_dataframe.csv")
+        self.paths_traversals_df = pd.read_csv(self.path_output_folder / "path_traversals_dataframe.csv")
---
+++ BISTRO-Optimization-Library/utilities/input_sampler.py
@@ -0,0 +1,288 @@
+"""Utility functions to sample random inputs for execution in Uber Prize.
+Example
+-------
+Provide the root to the data directory and select a scenario:
+{{
+    # By default, this is /reference-data in the root of this repository
+    ROOT_DATA_DIR = <path_to_data_dir_as_string>
+}}
+Get the mapping of agencies to paths for this :
+{{
+    SCENARIO_NAME = 'sioux_faux'
+    agency_dict = scenario_agencies(DATA_DIR, SCENARIO_NAME)
+}}
+Here's how to generate a sample of 5 records from each of the input samplers for an agency as
+separate Pandas `DataFrame`s. For illustrative purposes, we only one agency (the only one in
+Sioux Faux), but you can just iterate through the agency mapping if more agencies are desired:
+    {{
+    agency = 'sioux_faux_bus_lines'
+    agency_dict = scenario_agencies(DATA_DIR,{}.format(SCENARIO_NAME))
+    # Create a lazy cache of GTFS data for the agency:
+    sf_gtfs_manager = AgencyGtfsDataManager(agency_dict[agency])
+    freq_df = sample_frequency_adjustments(num_records, sf_gtfs_manager)
+    mode_incentive_df = sample_mode_incentives(num_records)
+    vehicle_fleet_mix_df = sample_vehicle_fleet_mix(num_records, sf_gtfs_manager)
+    }}
+Write each dataframe of samples to file using <input_df>.to_csv(<filename>, index=None) if desired.
+"""
+
+from collections import Counter
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from utils import lazyprop
+
+MASS_TRANSIT_FARE_FILE = "MassTransitFares.csv"
+
+AGE_RANGE_LOWER = np.array([i for i in range(1, 116, 5)])
+AGE_RANGE_UPPER = np.array([i for i in range(5, 120, 5)])
+INCOME_RANGE_LOWER = np.array([0] + [i for i in range(5000, 150000, 5000)])
+INCOME_RANGE_UPPER = np.array([i for i in range(4999, 149999, 5000)] + [150000])
+
+
+def scenario_agencies(data_dir, scenario_name):
+    """Given root data directory and scenario name, computes a mapping
+    of agency names to their respective paths.
+    Parameters
+    ----------
+    data_dir : Path
+        Absolute path to root of data directory
+    scenario_name : str
+        Name of scenario with GTFS data
+    Returns
+    -------
+    dict
+        Dictionary of agency names mapped to directories containing files comprising their GTFS
+        data.
+    """
+    gtfs_root = (data_dir / scenario_name).absolute()
+    return {p.stem: p for p in
+            gtfs_root.iterdir()}
+
+
+class AgencyGtfsDataManager(object):
+
+    def __init__(self, agency_gtfs_path):
+        """Used to cache an agency's GTFS data for sampling purposes
+        Parameters
+        ----------
+        agency_gtfs_path : pahtlib.Path object
+            Directory containing the agency's gtfs data
+        """
+
+        self.agency_gtfs_path = agency_gtfs_path
+
+    @lazyprop
+    def routes(self):
+        return pd.read_csv(self.agency_gtfs_path / "gtfs_data/routes.txt", header=0, index_col=1,
+                           na_values=None,
+                           delimiter=',')
+
+    @lazyprop
+    def vehicle_types(self):
+        return pd.read_csv(self.agency_gtfs_path / "availableVehicleTypes.csv", header=0,
+                           index_col=0, na_values=None,
+                           delimiter=',')
+
+    @lazyprop
+    def trips(self):
+        return pd.read_csv(self.agency_gtfs_path / "gtfs_data/trips.txt", header=0, index_col=2,
+                           na_values=None,
+                           delimiter=',')
+
+
+def sample_vehicle_fleet_mix_input(num_records, gtfs_manager, bus_set=None):
+    """Generate random `VehicleFleetMix` input according to possible substitute
+    vehicle trip ids available for an agency.
+    Parameters
+    ----------
+    num_records : int
+        Number of randomly sampled records to create.
+    gtfs_manager : `AgencyGtfsDataManager`
+        An instance of the `AgencyGtfsDataManager` for the target agency.
+    bus_set : list of strings
+        A list of possible bus types that we want to sample from if we don't want to sample from all
+        bus types. If bus_set = None samples from all bus types
+    Returns
+    -------
+    `pd.DataFrame`
+        `num_records` `VehicleFleetMix` records. These are unique by `routeId`
+        for the `agencyId` specified on the `gtfs_manager`
+    Raises
+    ------
+    `ValueError`
+        If the `num_records` is in excess of the number of routes that an agency schedules buses on.
+    """
+    df_columns = ["agencyId", "routeId", "vehicleTypeId"]
+    if num_records == 0:
+        return pd.DataFrame({k: [] for k in df_columns})
+
+    max_num_routes = gtfs_manager.routes.shape[0]
+    if num_records > max_num_routes:
+        raise ValueError(
+            "More samples requested than the number of routes available in agency; please enter a "
+            "number less than {}".format(
+                max_num_routes))
+    route_agency_sample = gtfs_manager.routes.sample(num_records)
+    routes = pd.Series(route_agency_sample.index.values)
+    agency = pd.Series(route_agency_sample.agency_id.values)
+    if bus_set is None:
+        vehicles = pd.Series((gtfs_manager.vehicle_types.filter(like="BUS", axis=0))
+                             .sample(num_records, replace=True).index)
+    else:
+        vehicles = pd.Series(bus_set).sample(num_records, replace=True).reset_index(drop=True)
+    df = pd.concat([agency, routes, vehicles], axis=1, ignore_index=True)
+    df.columns = df_columns
+    return df
+
+
+def _sample_census_interval(census_type):
+    if census_type == "age":
+        eps = AGE_RANGE_LOWER, AGE_RANGE_UPPER
+    elif census_type == "income":
+        eps = INCOME_RANGE_LOWER, INCOME_RANGE_UPPER
+    else:
+        raise ValueError("Undefined census type!")
+    right_ep = np.random.choice(eps[1])
+    left_ep = np.random.choice(eps[0][eps[0] < right_ep])
+    return "[{}:{}]".format(left_ep, right_ep)
+
+
+def sample_frequency_adjustment_input(num_service_periods, gtfs_manager):
+    """Generate random `FrequencyAdjustment` inputs according to trips run by
+    an agency.
+    Creates `num_records` frequency adjustment records where fields for each record where the
+    `headway_secs` field is randomly chosen from a range of between `min_headway_seconds`
+    (per the route.txt file in the corresponding gtfs data for the trip) and 7200
+    seconds at intervals of 60 seconds and the `min_time` and `max_time` field
+    are sampled between the 0 and 86340 seconds, respectively (i.e.,
+    the possible minimum and maximum number of seconds in a day of 86359
+    seconds, given the headway interval).
+    Note that a frequency adjustment is really tied to a route based on a particular trip.
+    The trip serves as a template for the frequency adjustment. See the documentation
+    for further details.
+    Parameters
+    ----------
+    num_service_periods : int
+        Number of service periods with a new headway that can be added to a route.
+    gtfs_manager : `AgencyGtfsDataManager`
+        An instance of the `AgencyGtfsDataManager` for the target agency.
+    Returns
+    -------
+    `pd.DataFrame`
+        `num_records` `FrequencyAdjustmentInput` records.
+    """
+    if num_service_periods > 5:
+        raise ValueError(
+            "The maximum number of service periods per route is equal to {0} although it should not exceed 5.".format(
+                num_service_periods))
+
+    df_columns = ['route_id', 'start_time', 'end_time', 'headway_secs']
+
+    grouped = [np.random.randint(num_service_periods) * [route_id] for route_id in
+               gtfs_manager.routes.index.values.astype(int)]
+    route_id_list = [i for j in grouped for i in j]
+
+    min_secs = 0
+    max_secs = 86399
+    min_headway_seconds = 180
+    max_headway_seconds = 7199
+
+    frequency_data = []
+    route_frequency = Counter(route_id_list).items()
+    for route_id, route_num_service_periods in route_frequency:
+        st_et_flat = np.sort(
+            np.random.choice(np.arange(min_secs, max_secs, 60), route_num_service_periods, replace=False))
+
+        for st, et in zip(st_et_flat, st_et_flat[1:]):
+            headway = np.random.choice(np.arange(min_headway_seconds, max_headway_seconds, 60))
+            frequency_data.append([route_id, st, et, headway])
+
+    frequency_adjustment_df = pd.DataFrame(frequency_data, columns=df_columns)
+    frequency_adjustment_df['exact_times'] = 0
+    return frequency_adjustment_df
+
+
+def sample_mode_incentives_input(num_records, gtfs_manager=None, min_incentive=0.1, max_incentive=50):
+    """Generate random mode incentives inputs based on modes available for
+    subsidies.
+    Creates `num_records` ModeIncentivesInput records where fields for each record
+    are randomly sampled as follows:
+        * `age` : an interval with age values sampled uniformly from [1 .. 116]\cup[120] inclusive, in steps of 5.
+        * `mode` : uniformly from list of available modes for scenario.
+        * `income` : an interval with income values sampled uniformly from integers values
+           [0 to 144,999]\cup[150,000] in steps of 5,000.
+        * `amount` : uniformly from `range(0.1,50)`.
+    The amount of subsidy is rounded to the nearest $0.10.
+    Parameters
+    ----------
+    num_records : int
+        Number of randomly sampled records to create.
+    gtfs_manager : `AgencyGtfsDataManager`, optional
+        An instance of the `AgencyGtfsDataManager` for the target agency.
+    min_incentive : float
+        Minimum amount accepted for an incentive according to the inputs specifications of the Starter-Kit
+    max_incentive : float
+        Maximum amount accepted for an incentive according to the inputs specifications of the Starter-Kit
+    Notes
+    -----
+    `gtfs_manager` added to support duck-typing this field.
+    Returns
+    -------
+    `DataFrame`
+        `num_records` `ModeIncentivesInput` records.
+    """
+    df_columns = ['mode', 'age', 'income', 'amount']
+    if num_records == 0:
+        return pd.DataFrame({k: [] for k in df_columns})
+    possible_modes = ['OnDemand_ride', 'walk_transit', 'drive_transit']
+    modes = np.random.choice(possible_modes, num_records).tolist()
+    ages = [_sample_census_interval("age") for _ in range(num_records)]
+    incomes = [_sample_census_interval("income") for _ in range(num_records)]
+    amounts = [np.round(np.random.uniform(min_incentive, max_incentive), 1) for _ in range(num_records)]
+    return pd.DataFrame(np.array([modes, ages, incomes, amounts]).T,
+                        columns=df_columns)
+
+
+def sample_mass_transit_fares_input(num_records, gtfs_manager, max_fare_amount=10.0):
+    """Generate `num_records` random `PtFares` for an
+    agency (specified within `gtfs_manager`) by randomly sampling age and fare amount.
+    The fare amount will not exceed the maximum fare amount and cannot be less than $0.10 (else,
+    there shouldn't have been a fare assigned in the first place).
+    The age will be sampled from 1 to 120 inclusive (maximum age in scenario).
+    Parameters
+    ----------
+    num_records : int
+        Number of randomly sampled records to create.
+    gtfs_manager : `AgencyGtfsDataManager`
+        An instance of the `AgencyGtfsDataManager` for the target agency.
+    max_fare_amount : float
+        The maximum fare amount that should be charged.
+    Returns
+    -------
+    `pd.DataFrame`
+        `num_records` `PtFares` records. These are unique by `routeId`
+        for the `agencyId` specified on the `gtfs_manager`
+    Raises
+    ------
+    `ValueError`
+        If the `num_records` is in excess of the number of routes that an agency schedules
+        buses on.
+    """
+
+    df_columns = ['agencyId', 'routeId', 'age', 'amount']
+    if num_records == 0:
+        return pd.read_csv('../submission-inputs/{0}'.format(MASS_TRANSIT_FARE_FILE))
+    max_num_routes = gtfs_manager.routes.shape[0]
+    if num_records > max_num_routes:
+        raise ValueError(
+            "More samples requested than the number of routes available in agency; please enter a "
+            "number less than {}".format(max_num_routes))
+    route_agency_sample = gtfs_manager.routes.sample(num_records)
+    routes = pd.Series(route_agency_sample.index.values)
+    agency = pd.Series(route_agency_sample.agency_id.values)
+    amounts = [np.round(np.random.uniform(0.1, max_fare_amount), 1) for _ in range(num_records)]
+    ages = [_sample_census_interval("age") for _ in range(num_records)]
+    return pd.DataFrame(np.array([agency, routes, ages, amounts]).T,
+                        columns=df_columns)
